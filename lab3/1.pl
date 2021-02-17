3235b04767f8dfaa2a44b1d6a22b4279095472b7
Unknown User <rvas2012@yandex.ru>
2021-02-17 16:37:28 +0300

    task 2

--- lab3/1.pl
+++ lab3/1.pl
@@ -1,7 +1,2 @@
-max(X,Y,Z):-X>Y, Z is X ; Y>X, Z is Y;write(Z).
-
-
-
-
-
-
+max(X,Y,Z):-X>=Y, Z is X ; Y>=X, Z is Y;write(Z).
+max(X,Y,U,Z):- X>Y,X>U, Z is X ,write(Z); Y>X,Y>U, Z is Y,write(Z); U>X,U>Y, Z is U, write(Z).
