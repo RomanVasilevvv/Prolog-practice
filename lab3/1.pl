05cfbce935232793150b9518da54a8f4dedab69c
Unknown User <rvas2012@yandex.ru>
2021-03-05 15:16:49 +0300

    13 task

--- lab3/1.pl
+++ lab3/1.pl
@@ -22,7 +22,7 @@ summ(0,0):-!.
 summ(X,Y):-X>0, X1 is (X mod 10),X2 is (X div 10), summ(X2,Y1), Y is Y1+X1.
 
 summ(0, CurX, CurX) :- !.
-summ(Y, CurX, X) :- Mod is Y mod 10, Y1 is Y div 10, CurX1 is CurX + Mod, numbers(Y1, CurX1, X).
+summ(Y, CurX, X) :- Mod is Y mod 10, Y1 is Y div 10, CurX1 is CurX + Mod, summ(Y1, CurX1, X).
 summdown(Y, X) :- summ(Y, 0, X).
 
 maxNum(X, X) :- X div 10 =:= 0, !.
@@ -40,7 +40,46 @@ divNum(Num, Div, Div) :- Num = 0,Div is CurDiv,!.
 divNum(Num, CurDiv, Div) :- Mod is (Num mod 10), Num1 is (Num div 10), (Mod == 3 -> CurDiv1 is (CurDiv + Mod); CurDiv1 is CurDiv), divNum(Num1, CurDiv1, Div).
 divNumN(Num, Div) :- divNum(Num, 0, Div), Div is Div.
 
-nodNums(X,Y,NOD):- X =:= 0,NOD is Y, !; Y =:= 0,NOD is X, !.
-nodNums(X,Y,NOD):- (X > Y -> X1 is X mod Y,nodNums(Y,X1,NOD);Y1 is Y mod X, nodNums(X,Y1,NOD)).
-nodNumsN(X,Y,NOD):- nodNums(X,Y,NOD), NOD is NOD.
+%nodNums(X,Y,NOD):- X =:= 0,NOD is Y, !; Y =:= 0,NOD is X, !.
+% nodNums(X,Y,NOD):- (X > Y -> X1 is X mod Y,nodNums(Y,X1,NOD);Y1 is Y
+% mod X, nodNums(X,Y1,NOD)).
+%nodNumsN(X,Y,NOD):- nodNums(X,Y,NOD), NOD is NOD.
+
+
+
+nodNums(Num1, Num1, Num1, Num1) :- !.
+nodNums(_, _, 1, 1) :- !.
+nodNumsN(0, _, -1) :- !.
+nodNumsN(_, 0, -1) :- !.
+nodNumsN(0, 0, -1) :- !.
+nodNums(Num1, Num2, CurNod, Nod) :- Num1 mod CurNod =:= 0, Num2 mod CurNod =:= 0, Nod is CurNod, !.
+nodNums(Num1, Num2, CurNod, Nod) :- CurNod1 is CurNod - 1, nodNums(Num1, Num2, CurNod1, Nod).
+nodNumsN(Num1, Num2, Nod) :- (Num1 > Num2 -> nodNums(Num1, Num2, Num2, Nod); nodNums(Num1, Num2, Num1, Nod)).
+
+
+simpNumFalse():-false,!.
+simpNum(X,DEL):-DEL =:= 1,!.
+simpNum(X,DEL):-(X mod DEL =:= 0 -> simpNumFalse(); DEL1 is (DEL - 1),simpNum(X,DEL1)).
+simpNumN(X):-DEL is (X - 1),simpNum(X,DEL).
+
+countSum(Count1):-Count1 = Count + 1.
+countDels(X,0,Count1):-!.
+countDels(X,DEL,Count1):-DEL =:= 1,Count1 is 0,!.
+countDels(X,DEL,Count1):-(X mod DEL =:= 0 -> DEL1 is (DEL - 1),countDels(X,DEL1,Count1), Count1 is (Count + 1); DEL1 is (DEL - 1),countDels(X,DEL1,Count1), Count1 is Count).
+countDelsN(X,Count1):-DEL is (X - 1),countDels(X,DEL,Count1).
+
+
+
+
+
+%13
+col(1,1):-!.
+col(2,2):-!.
+col(X,N):-X>2,X mod 2 =:= 0,X1 is (X//2),X=\=1,col(X1,Z),N is Z+1.
+col(X,N):-X>2,X mod 2 =\= 0,X1 is (3*X+1),X=\=1,col(X1,Z),N is Z+1.
+
+colN(Max,Max,100000):-!.
+colN(Max,MaxN,Num):-Num1 is (Num + 1),col(Num1,N),(N > Max -> Max1 is N,colN(Max1,MaxN,Num1);colN(Max,MaxN,Num1)).
+colN1(Max,MaxN,Num):- colN(Max,MaxN,Num),write(MaxN).
+
 
